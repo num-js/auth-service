b0275798b90756023ddbfac4c9cc80f6c4699b0b
Numan Ahmed <mdnmnahmed@gmail.com>
2021-10-27 19:02:13 +0530

    Package installation & Middlewares setup

--- app.js
+++ app.js
@@ -1,7 +1,33 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+require('dotenv/config');
 
 const app = express();
+
+//Middlewares
+app.use(bodyParser.json());
+app.use(cors());
+app.use('/api', require('./routes/authRoutes'))
+
+app.get('/', (req, res) => {
+    res.status(200).json({ message: 'Welcome in AUTH' })
+})
+
+//Connection with DB
+mongoose.connect(process.env.DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+})
+    .then((res) => {
+        console.log("Connected with MongoDB ");
+    })
+    .catch((err) => {
+        console.log("Error in with MongoDB ", err);
+    });
+
 
 app.get('/', (req, res) => {
     res.json({ message: 'Hello Num' });
@@ -10,4 +36,4 @@ app.get('/', (req, res) => {
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log('App Started');
-})
+});
